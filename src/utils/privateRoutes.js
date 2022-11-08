e688b070776ffc7038da499bb4d094048082c088
Franco Felicioni <fran.felicioni@gmail.com>
2022-11-08 23:04:31 +0100

    privateRoutes.js created

---
+++ src/utils/privateRoutes.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+
+import { Navigate, Outlet } from "react-router-dom";
+
+const PrivateRoutes = () => {
+
+  const LOCAL_AUTH = "authenticated";
+  const [auth, setAuth] = useState(JSON.parse(localStorage.getItem(LOCAL_AUTH)) !== null);
+
+  return auth ? <Outlet /> : <Navigate to="/login" />;
+};
+
+export default PrivateRoutes;
