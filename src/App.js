085721e6d28d37e0fafda3f87cb863e3b0508315
Franco Felicioni <fran.felicioni@gmail.com>
2022-11-10 09:51:11 +0100

    App.js  modified

--- src/App.js
+++ src/App.js
@@ -2,51 +2,61 @@ import "./App.css";
 
 import { Route, Routes } from "react-router-dom";
 
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
-import Room from "./pages/Room";
-import Rooms from "./pages/Rooms";
-import Bookings from "./pages/Bookings";
-import Guest from "./pages/Guest";
-import Guests from "./pages/Guests";
-import User from "./pages/User";
-import Users from "./pages/Users";
+import Login from "./components/Login/Login";
+import Dashboard from "./views/Dashboard/Dashboard";
+import Room from "./views/Room";
+import Rooms from "./views/Rooms";
+import Bookings from "./views/Bookings";
+import Guest from "./views/Guest";
+import Guests from "./views/Guests";
+import User from "./views/User";
+import Users from "./views/Users";
 import PrivateRoutes from "./utils/privateRoutes";
-import NotFound from "./pages/NotFound";
+import NotFound from "./views/NotFound";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
+      <Routes>
+        <Route path="/login" element={<Login />} />
 
-      <Route element={<privateRoutes />}>
-        <Route path="/" element={<Dashboard />}/>
-        <Route path="/dashboard/reviews" element={<Dashboard />} exact/>
-        <Route path="/dashboard/reviews/published" element={<Dashboard />} exact/>
-        <Route path="/dashboard/reviews/archived" element={<Dashboard />} exact/>
+        <Route element={<PrivateRoutes />}>
+          <Route exact path="/" element={<Dashboard />} />
+          <Route exact path="/dashboard/reviews" element={<Dashboard />} />
+          <Route
+            exact
+            path="/dashboard/reviews/published"
+            element={<Dashboard />}
+          />
+          <Route
+            exact
+            path="/dashboard/reviews/archived"
+            element={<Dashboard />}
+          />
 
-        <Route path="/rooms" element={<Rooms />} exact/>
-        <Route path="/rooms/active" element={<Rooms />} exact/>
-        <Route path="/rooms/inactive" element={<Rooms />} exact/>
-        <Route path="/rooms/new-room" element={<Rooms />} exact/>
-        <Route path="/room/:id" element={<Room />} exact/>
-        <Route path="/room/edit/:id" element={<Room />} exact/>
+          <Route exact path="/rooms" element={<Rooms />} />
+          <Route exact path="/rooms/active" element={<Rooms />} />
+          <Route exact path="/rooms/inactive" element={<Rooms />} />
+          <Route exact path="/rooms/new-room" element={<Rooms />} />
+          <Route exact path="/room/:id" element={<Room />} />
+          <Route exact path="/room/edit/:id" element={<Room />} />
 
-        <Route path="/bookings" element={<Bookings />} exact/>
+          <Route path="/bookings" element={<Bookings />} exact />
 
-        <Route path="/guests" element={<Guests />} />
-        <Route path="/guest/:id" element={<Guest />} exact/>
+          <Route exactpath="/guests" element={<Guests />} />
+          <Route exact path="/guest/:id" element={<Guest />} />
 
-        <Route path="/users" element={<Users />} exact/>
-        <Route path="/users/active" element={<Users />} exact/>
-        <Route path="/users/inactive" element={<Users />} exact/>
-        <Route path="/users/new-user" element={<Users />} exact/>
-        <Route path="/user/:id" element={<User />} exact/>
-        <Route path="/user/edit/:id" element={<User />} exact/>
+          <Route exact path="/users" element={<Users />} />
+          <Route exact path="/users/active" element={<Users />} />
+          <Route exact path="/users/inactive" element={<Users />} />
+          <Route exact path="/users/new-user" element={<Users />} />
+          <Route exact path="/user/:id" element={<User />} />
+          <Route exact path="/user/edit/:id" element={<User />} />
 
-        <Route path='*' element={<NotFound/>}>404 Not found</Route>
-      </Route>
-    </Routes>
+          <Route path="*" element={<NotFound />}>
+            404 Not found
+          </Route>
+        </Route>
+      </Routes>
   );
 }
 
