ea1fcdfbd576a758f167bbf55210cbf74850ad4f
Franco Felicioni <fran.felicioni@gmail.com>
2022-11-10 09:49:14 +0100

    Reviews component created

---
+++ src/components/Reviews/Reviews.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+import {
+  Container,
+  Title,
+  CardContainer,
+  Card,
+  Description,
+  CardInfoContainer,
+  CardInfo,
+  Image,
+  Name,
+  Time,
+  CardButtons,
+} from "./Reviews_sc";
+import { AiOutlineCheckCircle, AiOutlineCloseCircle } from "react-icons/ai";
+
+const Reviews = () => {
+  return (
+    <Container>
+      {/* <Title> Latest Review by Customers </Title> */}
+      <CardContainer>
+        <Card>
+          <Description>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam
+          </Description>
+          <CardInfoContainer>
+            <CardInfo>
+              <Image />
+              <div style={{ display: "flex", flexDirection: "column" }}>
+                <Name>Kusnaidi Anderson</Name>
+                <Time>4m ago</Time>
+              </div>
+            </CardInfo>
+            <CardButtons>
+              <AiOutlineCheckCircle />
+              <AiOutlineCloseCircle />
+            </CardButtons>
+          </CardInfoContainer>
+        </Card>
+      </CardContainer>
+    </Container>
+  );
+};
+
+export default Reviews;
---
+++ src/components/Reviews/Reviews_sc.jsx
@@ -0,0 +1,89 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    margin: 1rem;
+`;
+
+const Title = styled.h3`
+  text-align: left;
+  color: var(--color-grey39);
+  font-size: 20px;
+`;
+
+const CardContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+`;
+
+const Card = styled.div`
+  background: #ffffff 0% 0% no-repeat padding-box;
+  border: 1px solid #ebebeb;
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+`;
+const Description = styled.p`
+  width: 371px;
+  height: 107px;
+  text-align: left;
+  font-size: 16px;
+  letter-spacing: 0px;
+  color: var(--color-grey4E);
+`;
+
+const CardInfoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-top: 1.5rem;
+`;
+
+const CardInfo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`;
+
+const Image = styled.div`
+  width: 56px;
+  height: 56px;
+  background: var(--color-greyC5);
+  border-radius: 8px;
+`;
+
+const Name = styled.div`
+  color: var(--color-grey26);
+  text-align: left;
+  font-weight: 600;
+  font-size: 16px;
+`;
+
+const Time = styled.div`
+  width: 55px;
+  height: 20px;
+  text-align: left;
+  font-size: 14px;
+  letter-spacing: 0px;
+  color: var(--color-greenGrey);
+`;
+
+const CardButtons = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export {
+  Container,
+  Title,
+  CardContainer,
+  Card,
+  Description,
+  CardInfoContainer,
+  CardInfo,
+  Image,
+  Name,
+  Time,
+  CardButtons,
+};
