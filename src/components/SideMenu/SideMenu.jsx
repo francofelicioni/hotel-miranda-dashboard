f9a14e0d3d9338a0fa32276554d42ef6a12a8b7b
Franco Felicioni <fran.felicioni@gmail.com>
2022-11-10 09:48:19 +0100

    SideMenu component created

---
+++ src/components/SideMenu/SideMenu.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+
+import { MdOutlineDashboard } from "react-icons/md";
+import { AiOutlineCalendar } from "react-icons/ai";
+import { BiKey } from "react-icons/bi";
+import { BsPerson } from "react-icons/bs";
+import { FaRegUserCircle } from "react-icons/fa";
+
+import {
+  Container,
+  SideIconContainer,
+  IconContainer,
+  IconDesc,
+  Card,
+  Image,
+  CardTitle,
+  CardEmail,
+  Button,
+  Footer,
+  FooterTitle,
+  FooterCopyrigth,
+  FooterDescription,
+} from "./SideMenu_sc";
+import { Link } from "react-router-dom";
+
+const SideMenu = () => {
+  return (
+    <Container>
+      <SideIconContainer>
+        <Link to="/" style={{textDecoration: 'none', color: 'var(--color-greenGrey)'}}>
+          <IconContainer>
+            <MdOutlineDashboard />
+            <IconDesc>Dashboard</IconDesc>
+          </IconContainer>
+        </Link>
+        <Link to="/bookings" style={{textDecoration: 'none', color: 'var(--color-greenGrey)'}}>
+          <IconContainer>
+            <AiOutlineCalendar />
+            <IconDesc>Bookings</IconDesc>
+          </IconContainer>
+        </Link>
+        <Link to="/rooms" style={{textDecoration: 'none', color: 'var(--color-greenGrey)'}}>
+          <IconContainer>
+            <BiKey />
+            <IconDesc>Rooms</IconDesc>
+          </IconContainer>
+        </Link>
+        <Link to="/contact" style={{textDecoration: 'none', color: 'var(--color-greenGrey)'}}>
+          <IconContainer>
+            <BsPerson />
+            <IconDesc>Contact</IconDesc>
+          </IconContainer>
+        </Link>
+        {/* <Link to="/guest" style={{textDecoration: 'none', color: 'var(--color-greenGrey)'}}>
+          <IconContainer>
+            <BsPerson />
+            <IconDesc>Guest</IconDesc>
+          </IconContainer>
+        </Link> */}
+        <Link to='/users' style={{textDecoration: 'none', color: 'var(--color-greenGrey)'}}>
+          <IconContainer>
+            <FaRegUserCircle />
+            <IconDesc>Users</IconDesc>
+          </IconContainer>
+        </Link>
+      </SideIconContainer>
+      <Card>
+        <Image />
+        <CardTitle> Franco Felicioni</CardTitle>
+        <CardEmail> franco@hotelmiranda.com</CardEmail>
+        <Button>Edit</Button>
+      </Card>
+      <Footer>
+        <FooterTitle> HM Admin Dashboard</FooterTitle>
+        <FooterCopyrigth> © 2022 All Rights Reserved</FooterCopyrigth>
+        <FooterDescription> Made with ♥ by Fran</FooterDescription>
+      </Footer>
+    </Container>
+  );
+};
+
+export default SideMenu;
---
+++ src/components/SideMenu/SideMenu_sc.jsx
@@ -0,0 +1,125 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  /* display: flex; */
+  /* flex-direction: column; */
+  width: 250px;
+  height: 100vh;
+`;
+
+const SideIconContainer = styled.div`
+  padding: 2rem 2rem;
+`;
+
+const IconContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  padding: .5rem 0;
+`;
+const IconDesc = styled.p`
+  font-family: var(--font-main);
+  color: var(--color-greenGrey);
+  font-size: 18px;
+  font-weight: 600;
+
+  &-hover {
+    color: red;
+  }
+`;
+
+const Card = styled.div`
+  width: 233px;
+  height: 300px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+  box-shadow: 0px 20px 30px #00000014;
+  border-radius: 18px;
+  padding: 2rem 0;
+  margin: 0 auto; 
+`;
+
+const Image = styled.div`
+  width: 70px;
+  height: 70px;
+  background: #c5c5c5 0% 0% no-repeat padding-box;
+  border-radius: 8px;
+  opacity: 1;
+`;
+
+const CardTitle = styled.h2`
+  font: normal normal medium 16px/25px Poppins;
+  letter-spacing: 0px;
+  color: #393939;
+  opacity: 1;
+`;
+
+const CardEmail = styled.h3`
+  text-align: center;
+  font: normal normal 300 12px/18px Poppins;
+  letter-spacing: 0px;
+  color: #b2b2b2;
+  opacity: 1;
+`;
+
+const Button = styled.button`
+  width: 158px;
+  height: 47px;
+  background: var(--color-greyDC) 0% 0% no-repeat padding-box;
+  border-radius: 8px;
+  font-weight: 600;
+  font-size: 14px;
+  color: var(--color-greenDark);
+`;
+
+const Footer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 2rem;
+    gap: .5rem;
+`;
+
+const FooterTitle = styled.p`
+  /* width: 239px;
+  height: 23px; */
+  text-align: left;
+  font-weight: 600;
+  font-size: 16px;
+  color: #212121;
+`;
+
+const FooterCopyrigth = styled.p`
+  /* width: 239px;
+  height: 23px; */
+  text-align: left;
+  font-weight: 300;
+  font-size: 14px;
+  color: #799283;
+`;
+
+const FooterDescription = styled.p`
+  text-align: left;
+  font-weight: 300;
+  font-size: 14px;
+  color: #799283;
+  padding-top: 2rem;
+`;
+
+export {
+  Container,
+  SideIconContainer,
+  IconContainer,
+  IconDesc,
+  Card,
+  Image,
+  CardTitle,
+  CardEmail,
+  Button,
+  Footer,
+  FooterTitle,
+  FooterCopyrigth,
+  FooterDescription
+};
