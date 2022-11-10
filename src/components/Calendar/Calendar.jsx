e9a32fbce3466aa832d4dc81e68306935a513d85
Franco Felicioni <fran.felicioni@gmail.com>
2022-11-10 09:47:28 +0100

    Calendar component created

---
+++ src/components/Calendar/Calendar.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { useState } from "react";
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
