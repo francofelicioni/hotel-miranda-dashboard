ee57c7b3c7fb428f57962f6be83bea7810dbe7a2
Franco Felicioni <fran.felicioni@gmail.com>
2022-11-11 09:46:41 +0100

    App.js modified

--- src/App.js
+++ src/App.js
@@ -3,61 +3,89 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 
 import Login from "./components/Login/Login";
+
+import PrivateRoutes from "./utils/privateRoutes";
+
+import Header from "./components/Header/Header";
+import SideMenu from "./components/SideMenu/SideMenu";
 import Dashboard from "./views/Dashboard/Dashboard";
 import Room from "./views/Room";
-import Rooms from "./views/Rooms";
-import Bookings from "./views/Bookings";
+import Rooms from "./views/Rooms/Rooms";
+import Bookings from "./views/Bookings/Bookings";
 import Guest from "./views/Guest";
 import Guests from "./views/Guests";
 import User from "./views/User";
-import Users from "./views/Users";
-import PrivateRoutes from "./utils/privateRoutes";
-import NotFound from "./views/NotFound";
+import Users from "./views/Users/Users";
 
-function App() {
+import NotFound from "./views/NotFound/NotFound";
+
+import styled from "styled-components";
+import Contact from "./views/Contact/Contact";
+
+const RenderContainer = styled.div`
+  position: relative;
+  /* right: 20%;
+  top: 134px; */
+  /* right: 0px;
+  top: 0px; */
+`;
+
+function AppTest() {
   return (
-      <Routes>
-        <Route path="/login" element={<Login />} />
+    <>
+      {/* <BrowserRouter> */}
+      <Header />
+      <SideMenu />
+      <RenderContainer className="renderContainer">
+        <Routes>
+          <Route path="/login" element={<Login />} />
 
-        <Route element={<PrivateRoutes />}>
-          <Route exact path="/" element={<Dashboard />} />
-          <Route exact path="/dashboard/reviews" element={<Dashboard />} />
-          <Route
-            exact
-            path="/dashboard/reviews/published"
-            element={<Dashboard />}
-          />
-          <Route
-            exact
-            path="/dashboard/reviews/archived"
-            element={<Dashboard />}
-          />
+          <Route element={<PrivateRoutes />}>
+            <Route exact path="/" element={<Dashboard />} />
+            <Route
+              exact
+              path="/dashboard/reviews"
+              element={<Dashboard />}
+            />
+            <Route
+              exact
+              path="/dashboard/reviews/published"
+              element={<Dashboard />}
+            />
+            <Route
+              exact
+              path="/dashboard/reviews/archived"
+              element={<Dashboard />}
+            />
 
-          <Route exact path="/rooms" element={<Rooms />} />
-          <Route exact path="/rooms/active" element={<Rooms />} />
-          <Route exact path="/rooms/inactive" element={<Rooms />} />
-          <Route exact path="/rooms/new-room" element={<Rooms />} />
-          <Route exact path="/room/:id" element={<Room />} />
-          <Route exact path="/room/edit/:id" element={<Room />} />
+            <Route exact path="/rooms" element={<Rooms />} />
+            <Route exact path="/rooms/active" element={<Rooms />} />
+            <Route exact path="/rooms/inactive" element={<Rooms />} />
+            <Route exact path="/rooms/new-room" element={<Rooms />} />
+            <Route exact path="/room/:id" element={<Room />} />
+            <Route exact path="/room/edit/:id" element={<Room />} />
 
-          <Route path="/bookings" element={<Bookings />} exact />
+            <Route path="/bookings" element={<Bookings />} exact />
 
-          <Route exactpath="/guests" element={<Guests />} />
-          <Route exact path="/guest/:id" element={<Guest />} />
+            <Route exactpath="/guests" element={<Guests />} />
+            <Route exact path="/guest/:id" element={<Guest />} />
 
-          <Route exact path="/users" element={<Users />} />
-          <Route exact path="/users/active" element={<Users />} />
-          <Route exact path="/users/inactive" element={<Users />} />
-          <Route exact path="/users/new-user" element={<Users />} />
-          <Route exact path="/user/:id" element={<User />} />
-          <Route exact path="/user/edit/:id" element={<User />} />
+            <Route exact path="/users" element={<Users />} />
+            <Route exact path="/users/active" element={<Users />} />
+            <Route exact path="/users/inactive" element={<Users />} />
+            <Route exact path="/users/new-user" element={<Users />} />
+            <Route exact path="/user/:id" element={<User />} />
+            <Route exact path="/user/edit/:id" element={<User />} />
 
-          <Route path="*" element={<NotFound />}>
-            404 Not found
+            <Route exact path="/contact" element={<Contact />} />
+
+            <Route path="*" element={<NotFound />} />
           </Route>
-        </Route>
-      </Routes>
+        </Routes>
+      </RenderContainer>
+      {/* </BrowserRouter> */}
+    </>
   );
 }
 
-export default App;
+export default AppTest;
