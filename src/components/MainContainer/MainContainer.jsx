1849c9ab75341175a153e938d4ca79004770f588
Franco Felicioni <fran.felicioni@gmail.com>
2022-11-11 14:43:41 +0100

    MainContainer component modified

---
+++ src/components/MainContainer/MainContainer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+import styled from "styled-components";
+
+const Container = styled.div`
+  position: absolute;
+  width: ${(props) => (props.path === "/login" ? "100%" : "82%")};
+  top: ${(props) => (props.path === "/login" ? "0" : "5rem")};
+  right: 0;
+  padding: ${(props) => (props.path === '/login' ? '0' : '1.875rem')};
+  background-color: var(--color-greyF8);
+`;
+
+const MainContainer = ({ children }) => {
+  const { pathname } = useLocation();
+
+  return (
+    <Container class="mainContainer" path={pathname}>
+      {children}
+    </Container>
+  );
+};
+
+export default MainContainer;
