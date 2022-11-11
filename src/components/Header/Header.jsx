1cc16955810381b74a1239f26dd2b8fa66aa25a3
Franco Felicioni <fran.felicioni@gmail.com>
2022-11-11 12:15:40 +0100

    Header container modified

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import {
   Navbar,
@@ -16,25 +16,31 @@ import {
 import { AiOutlineSearch, AiOutlineBell } from "react-icons/ai";
 import { FaRegEnvelope, FaArrowsAltH } from "react-icons/fa";
 import { MdLogout } from "react-icons/md";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
 
-  const [title, setTitle] = useState ('Dashboard');
+  const [title, setTitle] = useState ('');
 
   const location = useLocation();
 
   console.log(location.pathname);
 
-  // if (location.pathname === '/bookings' ) {
-  //   setTitle('Bookings')
-  // } else if(location.pathname  === '/rooms') {
-  //   setTitle('Rooms')
-  // } else if(location.pathname  === '/contact') {
-  //   setTitle('Contact') 
-  // } else if (location.pathname  === '/users') {
-  //   setTitle('Rooms')
-  // };
+  useEffect (()=> {
+    if (location.pathname === '/' ) {
+      setTitle('Dashboard')
+    } else if (location.pathname === '/bookings' ) {
+      setTitle('Bookings')
+    } else if(location.pathname  === '/rooms') {
+      setTitle('Rooms')
+    } else if(location.pathname  === '/contact') {
+      setTitle('Contact') 
+    } else if (location.pathname  === '/users') {
+      setTitle('Rooms')
+    };
+  },[location])
+
+
 
   console.log('title', title)
 
@@ -43,35 +49,22 @@ const Header = () => {
     location.pathname != "/login" && (
       <Navbar>
         <Left>
-          <Logo src="/icons/hotelMiranda.svg" />
-          {/* <FaArrowsAltH /> */}
+          <Link to='/'><Logo src="/icons/hotelMiranda.svg" /></Link>
           <FaArrowsAltH style={{ width: "22px", height: "30px" }} />
         </Left>
 
         <Center>
-          {/* <Container> */}
-          <Title> Dashboard </Title>
-          {/* </Container> */}
+          <Title> {title} </Title>
           <SearchContainer>
             <Input />
-            {/* <Icon> */}
             <AiOutlineSearch style={{ width: "22px", height: "30px" }} />
-            {/* </Icon> */}
           </SearchContainer>
         </Center>
 
         <Rigth>
-          {/* <IconContainer> */}
-          {/* <Icon> */}
           <FaRegEnvelope style={{ width: "22px", height: "30px" }} />
-          {/* </Icon> */}
-          {/* <Icon> */}
           <AiOutlineBell style={{ width: "22px", height: "30px" }} />
-          {/* </Icon> */}
-          {/* <Icon> */}
           <MdLogout style={{ width: "22px", height: "30px" }} />
-          {/* </Icon> */}
-          {/* </IconContainer> */}
         </Rigth>
       </Navbar>
     )
