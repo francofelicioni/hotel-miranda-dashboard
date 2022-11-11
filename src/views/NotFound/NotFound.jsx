23aa4f8baf47846143ea1dc869da42a99cca3090
Franco Felicioni <fran.felicioni@gmail.com>
2022-11-11 09:45:45 +0100

    View Not Found  modified

---
+++ src/views/NotFound/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { NotFoundContainer } from "./NotFound_sc";
+
+const NotFound = () => {
+  return (
+    <NotFoundContainer>
+      <h1>Sorry, page not found</h1>
+    </NotFoundContainer>
+  );
+};
+
+export default NotFound;
