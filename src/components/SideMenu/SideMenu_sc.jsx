9aee05658610af4f877462a9740af4d55521dbf9
Franco Felicioni <fran.felicioni@gmail.com>
2022-11-11 14:44:02 +0100

    SideMenu styles modified

--- src/components/SideMenu/SideMenu_sc.jsx
+++ src/components/SideMenu/SideMenu_sc.jsx
@@ -30,7 +30,7 @@ const IconDesc = styled.p`
 
 const Card = styled.div`
   width: 233px;
-  height: 300px;
+  /* height: 300px; */
   display: flex;
   flex-direction: column;
   align-items: center;
