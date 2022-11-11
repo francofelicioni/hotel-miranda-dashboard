74f76bf036aca3c54dde7c3127414142fccc8bdc
Franco Felicioni <fran.felicioni@gmail.com>
2022-11-11 09:43:47 +0100

    Header component  modified

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import {
   Navbar,
@@ -16,42 +16,65 @@ import {
 import { AiOutlineSearch, AiOutlineBell } from "react-icons/ai";
 import { FaRegEnvelope, FaArrowsAltH } from "react-icons/fa";
 import { MdLogout } from "react-icons/md";
+import { useLocation } from "react-router-dom";
 
 const Header = () => {
+
+  const [title, setTitle] = useState ('Dashboard');
+
+  const location = useLocation();
+
+  console.log(location.pathname);
+
+  // if (location.pathname === '/bookings' ) {
+  //   setTitle('Bookings')
+  // } else if(location.pathname  === '/rooms') {
+  //   setTitle('Rooms')
+  // } else if(location.pathname  === '/contact') {
+  //   setTitle('Contact') 
+  // } else if (location.pathname  === '/users') {
+  //   setTitle('Rooms')
+  // };
+
+  console.log('title', title)
+
+
   return (
-    <Navbar>
-      <Left>
-        <Logo src="/icons/hotelMiranda.svg" />
-        {/* <FaArrowsAltH /> */}
-        <FaArrowsAltH style={{ width: "22px", height: "30px" }} />
-      </Left>
+    location.pathname != "/login" && (
+      <Navbar>
+        <Left>
+          <Logo src="/icons/hotelMiranda.svg" />
+          {/* <FaArrowsAltH /> */}
+          <FaArrowsAltH style={{ width: "22px", height: "30px" }} />
+        </Left>
 
-      <Center>
-        {/* <Container> */}
-          <Title> Dashboard</Title>
-        {/* </Container> */}
-        <SearchContainer>
-          <Input />
+        <Center>
+          {/* <Container> */}
+          <Title> Dashboard </Title>
+          {/* </Container> */}
+          <SearchContainer>
+            <Input />
+            {/* <Icon> */}
+            <AiOutlineSearch style={{ width: "22px", height: "30px" }} />
+            {/* </Icon> */}
+          </SearchContainer>
+        </Center>
+
+        <Rigth>
+          {/* <IconContainer> */}
           {/* <Icon> */}
-          <AiOutlineSearch style={{ width: "22px", height: "30px" }} />
+          <FaRegEnvelope style={{ width: "22px", height: "30px" }} />
           {/* </Icon> */}
-        </SearchContainer>
-      </Center>
-
-      <Rigth>
-        {/* <IconContainer> */}
-        {/* <Icon> */}
-        <FaRegEnvelope style={{ width: "22px", height: "30px" }} />
-        {/* </Icon> */}
-        {/* <Icon> */}
-        <AiOutlineBell style={{ width: "22px", height: "30px" }} />
-        {/* </Icon> */}
-        {/* <Icon> */}
-        <MdLogout style={{ width: "22px", height: "30px" }}/>
-        {/* </Icon> */}
-        {/* </IconContainer> */}
-      </Rigth>
-    </Navbar>
+          {/* <Icon> */}
+          <AiOutlineBell style={{ width: "22px", height: "30px" }} />
+          {/* </Icon> */}
+          {/* <Icon> */}
+          <MdLogout style={{ width: "22px", height: "30px" }} />
+          {/* </Icon> */}
+          {/* </IconContainer> */}
+        </Rigth>
+      </Navbar>
+    )
   );
 };
 
