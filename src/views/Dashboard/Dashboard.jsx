6cdd21b8e2be59e057394b58da83b3080b517b02
Franco Felicioni <fran.felicioni@gmail.com>
2022-11-11 09:45:15 +0100

    View Dashbaord modified

--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -1,30 +1,26 @@
 import React from "react";
 
-import { MainContainer, Container, ReviewsContainer } from "./Dashboard_sc";
+import { DashBoardContainer, InsightsCardsContainer,  ReviewsContainer } from "./Dashboard_sc";
 
-import Header from "../../components/Header/Header";
-import SideMenu from "../../components/SideMenu/SideMenu";
 import InsightsCards from "../../components/InsightsCards/InsightsCards";
 import Reviews from "../../components/Reviews/Reviews";
 
 const Dashboard = () => {
   return (
     <>
-      <Header />
-      <MainContainer>
-        <SideMenu />
-        <Container>
+      <DashBoardContainer>
+        <InsightsCardsContainer>
           <InsightsCards />
           <InsightsCards />
           <InsightsCards />
           <InsightsCards />
-          <ReviewsContainer>
-            <Reviews />
-            <Reviews />
-            <Reviews />
-          </ReviewsContainer>
-        </Container>
-      </MainContainer>
+        </InsightsCardsContainer>
+        <ReviewsContainer>
+          <Reviews />
+          <Reviews />
+          <Reviews />
+        </ReviewsContainer>
+      </DashBoardContainer>
     </>
   );
 };
--- src/views/Dashboard/Dashboard_sc.jsx
+++ src/views/Dashboard/Dashboard_sc.jsx
@@ -1,23 +1,29 @@
 import styled from "styled-components";
 
-const MainContainer = styled.div`
-  display: flex;
-  align-items: flex-start;
-  gap: 10%;
-  margin: 0 auto;
+const DashBoardContainer = styled.div`
+  position: absolute;
+  width: 80%;
+  right: 0;
+  bottom: 500px;
+  /* background-color: red; */
 `;
 
-const Container = styled.div`
+const InsightsCardsContainer = styled.div`
   display: grid;
-  align-items: center;
+  /* align-items: center; */
+  justify-content: center;
   grid-template-columns: repeat(4, 300px);
   gap: 2rem;
   padding-top: 2rem;
+  width: 100%;
+  height: 100%;
 `;
 
 const ReviewsContainer = styled.div`
   display: flex;
+  /* align-items: center; */
+  justify-content: center;
   gap: 1rem;
-`
+`;
 
-export { MainContainer, Container, ReviewsContainer };
+export { DashBoardContainer, InsightsCardsContainer, ReviewsContainer };
