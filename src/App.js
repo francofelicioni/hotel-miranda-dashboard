970bfe4b6abeb46b9f41b747bb17432be35bd4ad
Franco Felicioni <fran.felicioni@gmail.com>
2022-11-14 08:45:25 +0100

    App.js modified

--- src/App.js
+++ src/App.js
@@ -1,86 +1,84 @@
 import "./App.css";
 
-import { Route, Routes } from "react-router-dom";
+import React, { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 
-import Login from "./components/Login/Login";
+import Login from "./views/Login/Login";
 
-import PrivateRoutes from "./utils/privateRoutes";
+// import PrivateRoutes from "./utils/privateRoutes";
 
 import MainContainer from "./components/MainContainer/MainContainer";
 
 import Header from "./components/Header/Header";
 import SideMenu from "./components/SideMenu/SideMenu";
 import Dashboard from "./views/Dashboard/Dashboard";
-import Room from "./views/Room";
 import Rooms from "./views/Rooms/Rooms";
 import Bookings from "./views/Bookings/Bookings";
-import Guest from "./views/Guest";
-import Guests from "./views/Guests";
-import User from "./views/User";
 import Users from "./views/Users/Users";
 
 import NotFound from "./views/NotFound/NotFound";
 
-import styled from "styled-components";
 import Contact from "./views/Contact/Contact";
 
+function App() {
 
-function AppTest() {
+
+
+  const PrivateRoutes = () => {
+
+    const LOCAL_AUTH = "authenticated";
+    // const [auth, setAuth] = useState(
+      
+    return JSON.parse(localStorage.getItem(LOCAL_AUTH)) !== null ? <Outlet /> : <Navigate to="/login" />;
+
+    // return auth ? <Outlet /> : <Navigate to="/login" />;
+  }
+
   return (
     <>
-      {/* <BrowserRouter> */}
-      <Header />
-      <SideMenu />
-      <MainContainer className="renderContainer">
-        <Routes>
-          <Route path="/login" element={<Login />} />
+      <BrowserRouter>
+        <Header />
+        <SideMenu />
+        <MainContainer className="renderContainer">
+          <Routes>
+            <Route exact path="/login" element={<Login />} />
 
-          <Route element={<PrivateRoutes />}>
-            <Route exact path="/" element={<Dashboard />} />
-            <Route
-              exact
-              path="/dashboard/reviews"
-              element={<Dashboard />}
-            />
-            <Route
-              exact
-              path="/dashboard/reviews/published"
-              element={<Dashboard />}
-            />
-            <Route
-              exact
-              path="/dashboard/reviews/archived"
-              element={<Dashboard />}
-            />
+            <Route element={<PrivateRoutes />}>
+              <Route exact path="/" element={<Navigate to='/dashboard' replace />} />
+              <Route exact path="/dashboard" element={<Dashboard />} />
+              <Route exact path="/dashboard/reviews" element={<Dashboard />} />
+              <Route
+                exact
+                path="/dashboard/reviews/published"
+                element={<Dashboard />}
+              />
+              <Route
+                exact
+                path="/dashboard/reviews/archived"
+                element={<Dashboard />}
+              />
 
-            <Route exact path="/rooms" element={<Rooms />} />
-            <Route exact path="/rooms/active" element={<Rooms />} />
-            <Route exact path="/rooms/inactive" element={<Rooms />} />
-            <Route exact path="/rooms/new-room" element={<Rooms />} />
-            <Route exact path="/room/:id" element={<Room />} />
-            <Route exact path="/room/edit/:id" element={<Room />} />
+              <Route exact path="/rooms" element={<Rooms />} />
+              <Route exact path="/rooms/active" element={<Rooms />} />
+              <Route exact path="/rooms/inactive" element={<Rooms />} />
+              <Route exact path="/rooms/new-room" element={<Rooms />} />
 
-            <Route path="/bookings" element={<Bookings />} exact />
+              <Route path="/bookings" element={<Bookings />} exact />
 
-            <Route exactpath="/guests" element={<Guests />} />
-            <Route exact path="/guest/:id" element={<Guest />} />
+              <Route exact path="/users" element={<Users />} />
+              <Route exact path="/users/active" element={<Users />} />
+              <Route exact path="/users/inactive" element={<Users />} />
+              <Route exact path="/users/new-user" element={<Users />} />
 
-            <Route exact path="/users" element={<Users />} />
-            <Route exact path="/users/active" element={<Users />} />
-            <Route exact path="/users/inactive" element={<Users />} />
-            <Route exact path="/users/new-user" element={<Users />} />
-            <Route exact path="/user/:id" element={<User />} />
-            <Route exact path="/user/edit/:id" element={<User />} />
+              <Route exact path="/contact" element={<Contact />} />
 
-            <Route exact path="/contact" element={<Contact />} />
-
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
-      </MainContainer>
-      {/* </BrowserRouter> */}
+              <Route path="*" element={<NotFound />} />
+            </Route>
+          </Routes>
+        </MainContainer>
+      </BrowserRouter>
     </>
   );
 }
-
-export default AppTest;
+export default App;
