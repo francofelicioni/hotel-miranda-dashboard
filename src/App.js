b00acff29c1f3b14fb6ce04539fce9aee78e344c
Franco Felicioni <fran.felicioni@gmail.com>
2022-11-17 18:30:25 +0100

    App.js modified

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Navigate, Outlet } from "react-router-dom";
 
