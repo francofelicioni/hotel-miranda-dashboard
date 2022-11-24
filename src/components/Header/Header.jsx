72cef690093644c8c6b41bea6c3b7e421a43d040
Franco Felicioni <fran.felicioni@gmail.com>
2022-11-24 12:26:26 +0100

    Changing the way that header's title renders

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -17,35 +17,34 @@ import { MdLogout } from "react-icons/md";
 import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
-
-  const [title, setTitle] = useState ('');
   const location = useLocation();
 
-
-  useEffect (()=> {
-    if (location.pathname === '/' ) {
-      setTitle('Dashboard')
-    } else if (location.pathname === '/bookings' ) {
-      setTitle('Bookings')
-    } else if(location.pathname  === '/rooms') {
-      setTitle('Rooms')
-    } else if(location.pathname  === '/contact') {
-      setTitle('Contact') 
-    } else if (location.pathname  === '/users') {
-      setTitle('Users')
-    };
-  },[location])
+  const setTitle = () => {
+    if (location.pathname === "/dashboard") {
+      return "Dashboard"
+    } else if (location.pathname === "/bookings") {
+      return "Bookings"
+    } else if (location.pathname === "/rooms") {
+      return "Rooms"
+    } else if (location.pathname === "/contact") {
+      return "Contact";
+    } else if (location.pathname === "/users") {
+      return "Users";
+    }
+  };
 
   return (
     location.pathname !== "/login" && (
       <Navbar>
         <Left>
-          <Link to='/'><Logo src="/icons/hotelMiranda.svg" /></Link>
+          <Link to="/">
+            <Logo src="/icons/hotelMiranda.svg" />
+          </Link>
         </Left>
 
         <Center>
           <FaArrowsAltH style={{ width: "22px", height: "30px" }} />
-          <Title> {title} </Title>
+          <Title> {setTitle()} </Title>
           <SearchContainer>
             <Input />
             <AiOutlineSearch style={{ width: "22px", height: "30px" }} />
