612287248cb3511b05bf904e571fbe03da7117a8
Franco Felicioni <fran.felicioni@gmail.com>
2022-11-25 09:12:24 +0100

    Not found view modified

--- src/views/NotFound/NotFound.jsx
+++ src/views/NotFound/NotFound.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import MainContainer from "../../components/MainContainer/MainContainer";
 
 import { NotFoundContainer } from "./NotFound_sc";
 
 const NotFound = () => {
   return (
-    <NotFoundContainer>
-      <h1>Sorry, page not found</h1>
-    </NotFoundContainer>
+    <MainContainer>
+      <NotFoundContainer>
+        <h1>Sorry, page not found</h1>
+      </NotFoundContainer>
+    </MainContainer>
   );
 };
 
