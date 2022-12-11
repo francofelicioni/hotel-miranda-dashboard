73fac5962395f89e63a442d531a905f3008ffc78
Franco Felicioni <fran.felicioni@gmail.com>
2022-12-11 20:06:22 +0100

    bookingSlice modified

--- src/features/bookings/bookingsSlice.js
+++ src/features/bookings/bookingsSlice.js
@@ -1,7 +1,8 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import fetchFrom from "../../utils/fetchFrom";
 
-export const fetchBookings = createAsyncThunk("bookings/fetchBookings",
+export const fetchBookings = createAsyncThunk(
+  "bookings/fetchBookings",
 
   async () => {
     const response = await fetchFrom("guests");
@@ -9,10 +10,36 @@ export const fetchBookings = createAsyncThunk("bookings/fetchBookings",
   }
 );
 
+export const fetchBooking = createAsyncThunk(
+  "booking/fetchBooking",
+
+  async (id) => {
+    const response = await fetchFrom("guests");
+    const result = response.filter(guest => guest.id === id);
+    return result;
+  }
+);
+
 const initialState = {
   bookings: [],
-  singleBooking: {},
-  status: 'idle',
+  singleBooking: {
+    id: "9f073bc2-0274-4f10-bd36-4eef756ef1ae",
+    image: "https://i.pravatar.cc/150?img=70",
+    full_name: "Dean Daniel",
+    order_date: "11/09/2021",
+    check_in: "01/01/2022",
+    check_out: "07/01/2022",
+    special_request:
+      "mollit consect elit cillum proident esse voluptate commodo ulla",
+    room_info: {
+      number: 0,
+      type: "Double",
+    },
+    price: "895,86 €",
+    facilities: [],
+    state: "Check In",
+  },
+  status: "idle",
 };
 
 export const bookingsSlice = createSlice({
@@ -22,21 +49,26 @@ export const bookingsSlice = createSlice({
   extraReducers: (builder) => {
     builder
       .addCase(fetchBookings.pending, (state) => {
-        state.status = 'pending';
+        state.status = "pending";
         console.log("Loading...");
       })
       .addCase(fetchBookings.fulfilled, (state, action) => {
         state.bookings = action.payload;
-        state.status = 'fulfilled';
+        state.status = "fulfilled";
       })
       .addCase(fetchBookings.rejected, (state) => {
-        state.status = 'rejected';
+        state.status = "rejected";
         console.log("Failure while fetching the requested data!");
-      });
+      })
+      .addCase(fetchBooking.fulfilled, (state, action) => {
+        console.log('ACTION', action)
+				state.singleBooking = action.payload[0];
+				state.status = 'ok';
+			})
   },
 });
 
 export default bookingsSlice.reducer;
 export const selectBookings = (state) => state.bookings.bookings;
+export const selectBooking = (state) => state.bookings.singleBooking;
 export const bookingsStatus = (state) => state.bookings.status;
-
