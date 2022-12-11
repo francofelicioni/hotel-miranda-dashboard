0142eb337acc03ca9fff26de2ef7699ad2bf57a9
Franco Felicioni <fran.felicioni@gmail.com>
2022-12-11 20:06:40 +0100

    Calendar and stats colors created

---
+++ src/styles/colors.js
@@ -0,0 +1,19 @@
+export const colors = {
+    green: "#799283",
+    hardGreen: "#135846",
+    lightGreen: "#EBF1EF",
+    red: "#E23428",
+    lightRed: "#FFEDEC",
+    light: "#B2B2B2",
+    bgGray: "#F8F8F8",
+    gray: "#6E6E6E",
+    borderGray: "#D4D4D4",
+    white: "white",
+    yellow: "#FFFF4E",
+    lightYellow: "#ffffcc",
+    hardYellow: "#a4a403",
+    lightViolet: "#eee6ff",
+    hardViolet: "#884dff",
+    redLess: "#e0564c",
+  };
+  
