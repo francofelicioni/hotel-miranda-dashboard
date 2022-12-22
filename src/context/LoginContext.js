c7317cbfac014653e9af9ffc432b2ddf78a1b2fd
Franco Felicioni <fran.felicioni@gmail.com>
2022-12-22 21:16:28 +0100

    Fixing authentication logout problem

--- src/context/LoginContext.js
+++ src/context/LoginContext.js
@@ -4,9 +4,8 @@ const LOCAL_AUTH = "authenticated";
 const userLogged = JSON.parse(localStorage.getItem(LOCAL_AUTH));
 
 export const initialState = {
-  isAuth: userLocalStorage.isAuth ? userLocalStorage.isAuth : false,
-	user: userLocalStorage ? { ...userLocalStorage.user } : { name: '', email:'' }
+  isAuth: userLogged.isAuth ? userLogged.isAuth : false,
+  user: userLogged ? { ...userLogged.user } : { name: "", email: "" },
 };
 
 export const LoginContext = createContext([]);
-
